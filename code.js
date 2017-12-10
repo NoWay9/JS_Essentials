3e98f552020e6712d5b0392b2e3517e1bc522aaa
Liubomyr <strukliubomyr@gmail.com>
2017-12-10 18:20:36 +0200

    Commit Index.html and code.js

---
+++ code.js
@@ -0,0 +1,26 @@
+var name, time, place, $event;
+$event = prompt('Enter event:', 'meeting');
+
+function valid(form) {
+name=form.name.value;
+time=form.time.value;
+place=form.place.value;
+if (name != '' && time != '' && place != '')
+console.log(name + ' has a ' + $event + ' today at ' + time + ' somewhere in ' + place);
+else alert('Input all data');
+}
+
+
+function convert() {
+var euro, dollar, result, result2, result3;
+euro = prompt('Enter value euro:', '100');
+dollar = prompt('Enter value dollar', '100');
+var moneyRate={
+euroRate: 31.789,
+dollarRate: 27.005
+};
+result= euro * moneyRate.euroRate;
+result2=dollar * moneyRate.dollarRate;
+result3= moneyRate.euroRate/moneyRate.dollarRate;
+alert(euro + ' euros are equal ' + result.toFixed(2) + ' grns, ' + dollar + ' dollars are equal ' + result2.toFixed(2) + ', one euro is equal ' + result3.toFixed(3) + ' dollars');
+}
